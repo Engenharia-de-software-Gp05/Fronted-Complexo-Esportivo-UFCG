80dd524e0bac090278537b6bdca45d984f6968e9
VictorFreir <victor@inwound.com>
2024-03-13 16:26:09 -0300

    implementa redefinir senha

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OTP from "./pages/otp";
 import Teste from "./pages/teste";
+import RedefinirSenha from "./pages/redefinir-senha"
+import NovaSenha from "./pages/redefinir-senha/nova_senha"
+
 
 function Rotas() {
   return (
@@ -10,6 +13,8 @@ function Rotas() {
         e
         <Route path="/otp" element={<OTP />} />
         <Route path="/" element={<Teste />} />
+        <Route path="/redefinir-senha" element={<RedefinirSenha />}/>
+        <Route path="/nova-senha" element={<NovaSenha />}> </Route>
       </Routes>
     </Router>
   );
