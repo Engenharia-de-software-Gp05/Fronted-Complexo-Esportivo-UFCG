4578308214b5c3c8094a638b8ca2f12cfa1d87f7
Matheus Victor <matheusvictor.dev@gmail.com>
2024-03-23 16:08:01 -0300

    fix: rename routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OTP from "./pages/otp";
-import RedefinirSenha from "./pages/redefinir-senha"
-import NovaSenha from "./pages/redefinir-senha/nova_senha"
-import OkEmailPage from "./pages/email-check"
-import ErrorPage from "./pages/wrong-code"
-import RedefinePasswordCheck from "./pages/new-password-check"
-
+import RedefinePassword from "./pages/redefine-password";
+import NewPassword from "./pages/redefine-password/new-password";
+import OkEmailPage from "./pages/email-check";
+import ErrorPage from "./pages/wrong-code";
+import RedefinePasswordCheck from "./pages/new-password-check";
+import RegisterCourt from "./pages/court/registerCourt";
 
 function Rotas() {
   return (
     <Router>
       <Routes>
         <Route path="/otp" element={<OTP />} />
-        <Route path="/redefinir-senha" element={<RedefinirSenha />}/>
-        <Route path="/nova-senha" element={<NovaSenha />}> </Route>
+        <Route path="/redefine-password" element={<RedefinePassword />}/>
+        <Route path="/new-password" element={<NewPassword />}> </Route>
         <Route path="/email-check" element={<OkEmailPage />} />
         <Route path="/wrong-code" element={<ErrorPage />} />
         <Route path="/new-password-check" element={<RedefinePasswordCheck />} />
+        <Route path="/register-court" element={<RegisterCourt />} />
+        {/* <Route path="/listar-quadras" element={<ListarQuadras/>} /> */}
       </Routes>
     </Router>
   );
