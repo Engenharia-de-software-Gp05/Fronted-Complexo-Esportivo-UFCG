ab6f8d714fd07f4e56151508937be422ba32dbc3
Matheus Victor <matheusvictor.dev@gmail.com>
2024-03-29 11:37:14 -0300

    refactor

--- src/pages/employee/listEmployees.js
+++ src/pages/employee/listEmployees.js
@@ -31,24 +31,39 @@ export default function ListEmployees() {
 
   const handleCloseDialog = () => {
     setOpenDialog(false);
-  }
+  };
 
   const handleDeleteUser = () => {
     //requisicao
     setSelectedUser(null);
     setOpenDialog(false);
-  }
+  };
 
   return (
-    <Container component="section" sx={{ width: 1100, height: 956, mt: 5 }}>
+    <Container
+      name="principal"
+      component="section"
+      item
+      xs={12}
+      sm={6}
+      style={{ maxWidth: 1000, maxHeight: 956, marginTop: 20 }}
+    >
       <CssBaseline>
-        <Typography variant="h3" sx={{ margin: 3 }}>
+        <Typography variant="h4" sx={{ margin: 3 }}>
           Lista de Funcionários
         </Typography>
-        <Grid container spacing={2} style={{ maxHeight: 880, height: 750 }}>
-          <Grid item xs={6} style={{ height: 750 }}>
+        <Grid container spacing={2} style={{ maxHeight: 880 }}>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            style={{ maxHeight: 750, position: "relative" }}
+            order={{ xs: 0, md: 1 }}
+            sx={{ p: 8 }}
+          >
             <div>
               <SearchBarCustom
+                fullwidth
                 database={database}
                 searchFor={"name"}
                 onSelectItem={handleSelectUser}
@@ -57,8 +72,16 @@ export default function ListEmployees() {
           </Grid>
           <Grid
             item
-            xs={6}
-            sx={{ bgcolor: "#FFF8F6", placeItems: "center", borderRadius: 5 }}
+            xs={12}
+            md={6}
+            sx={{
+              bgcolor: "#FFF8F6",
+              placeItems: "center",
+              borderRadius: 5,
+              border: "1px solid #ccc",
+              bgcolor: "common.white",
+            }}
+            order={{ xs: 0, md: 1 }}
           >
             <div
               className="rigth-section"
@@ -73,11 +96,10 @@ export default function ListEmployees() {
                     alignItems: "left",
                     justifyContent: "space-evenly",
                     height: 400,
+                    width: "100%",
                   }}
                 >
-                  <h3 style={{ fontStyle: "normal", fontSize: 32 }}>
-                    Detalhes do usuário
-                  </h3>
+                  <Typography variant="h5">Detalhes do Usuário</Typography>
                   <text>
                     <PersonOutlineIcon />
                     {selectedUser ? selectedUser.name : "Nome"}
@@ -95,14 +117,22 @@ export default function ListEmployees() {
                     {selectedUser ? selectedUser.phone : "Telfone"}
                   </text>
                 </div>
-                  <div style={{ display: 'flex', justifyContent: 'center'}}>
-                    <Button 
-                      type="submit"
-                      variant="contained"
-                      sx={{ borderRadius: 20, width: 212, height: 40, textTransform: 'none', margin: 5 }}
-                      onClick={handleOpenDialog}
-                    >Deletar Funcionário</Button>
-                  </div>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{
+                      borderRadius: 20,
+                      width: 212,
+                      height: 40,
+                      textTransform: "none",
+                      margin: 5,
+                    }}
+                    onClick={handleOpenDialog}
+                  >
+                    Deletar Funcionário
+                  </Button>
+                </div>
               </div>
             </div>
           </Grid>
@@ -112,11 +142,17 @@ export default function ListEmployees() {
       <Dialog open={openDialog} onClose={handleCloseDialog}>
         <DialogTitle>Confirmação de Exclusão</DialogTitle>
         <DialogContent>
-          <DialogContentText>Tem certeza que deseja excluir este Funcionário?</DialogContentText>
+          <DialogContentText>
+            Tem certeza que deseja excluir este Funcionário?
+          </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleCloseDialog} color='primary'>Cancelar</Button>
-          <Button onClick={handleDeleteUser} color='primary'>Confirmar</Button>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleDeleteUser} color="primary">
+            Confirmar
+          </Button>
         </DialogActions>
       </Dialog>
     </Container>
