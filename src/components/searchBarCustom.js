6840f9e2c9f607a2a5eb3419e1d0baf3bd96b4cc
Matheus Victor <matheusvictor.dev@gmail.com>
2024-03-29 23:33:01 -0300

    feat: create component searchBarCuston

--- src/components/searchBarCustom.js
+++ src/components/searchBarCustom.js
@@ -5,29 +5,6 @@ import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import SearchOutlined from '@mui/icons-material/SearchOutlined';
 import CancelOutlined from '@mui/icons-material/CancelOutlined';
 
-const database1 = [
-  { id: 1, name: 'João', email: 'joao@example.com', phone: '123456789' },
-  { id: 2, name: 'Maria', email: 'maria@example.com', phone: '987654321' },
-  { id: 3, name: 'Pedro', email: 'pedro@example.com', phone: '456123789' },
-  { id: 4, name: 'Ana', email: 'ana@example.com', phone: '789456123' },
-  { id: 5, name: 'Lucas', email: 'lucas@example.com', phone: '321654987' },
-  { id: 6, name: 'Mariana', email: 'mariana@example.com', phone: '654987321' },
-  { id: 7, name: 'Carlos', email: 'carlos@example.com', phone: '987321654' },
-  { id: 8, name: 'Fernanda', email: 'fernanda@example.com', phone: '654321987' },
-  { id: 9, name: 'Gabriel', email: 'gabriel@example.com', phone: '987654123' },
-  { id: 10, name: 'Juliana', email: 'juliana@example.com', phone: '321789456' },
-  { id: 11, name: 'Rafael', email: 'rafael@example.com', phone: '456789123' },
-  { id: 12, name: 'Amanda', email: 'amanda@example.com', phone: '789123456' },
-  { id: 13, name: 'Diego', email: 'diego@example.com', phone: '321456789' },
-  { id: 14, name: 'Laura', email: 'laura@example.com', phone: '456789321' },
-  { id: 15, name: 'Rodrigo', email: 'rodrigo@example.com', phone: '789321654' },
-  { id: 16, name: 'Camila', email: 'camila@example.com', phone: '654123789' },
-  { id: 17, name: 'Eduardo', email: 'eduardo@example.com', phone: '321789654' },
-  { id: 18, name: 'Isabela', email: 'isabela@example.com', phone: '789654321' },
-  { id: 19, name: 'Bruno', email: 'bruno@example.com', phone: '654321789' },
-  { id: 20, name: 'Fernando', email: 'fernando@example.com', phone: '987321456' },
-];
-
 const SearchBarCustom = ({ database, searchFor, onSelectItem }) => {
   const [query, setQuery] = useState('');
   const [filteredData, setFilteredData] = useState(database);
