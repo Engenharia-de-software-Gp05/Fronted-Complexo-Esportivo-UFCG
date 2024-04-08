458d6c99d5164951bbf02f51bd0bb5217076ace7
Marcos Antônio <marcoscardoso.programmer@gmail.com>
2024-04-08 18:08:46 -0300

    feat(Scheduler): Agora não é possível criar ou remarcar agendamentos sobrepondo outros.

--- src/components/scheduler/AppointmentEditor.js
+++ src/components/scheduler/AppointmentEditor.js
@@ -1,20 +1,47 @@
 import { EditingState } from "@devexpress/dx-react-scheduler";
 
-const AppointmentEditor = ({ datas, setData }) => {
+const AppointmentEditor = ({ setData, setAlert }) => {
+  const checkOverlap = (newAppointment, existingAppointments) => {
+    const sameDayAppointments = existingAppointments.filter(
+      (appointment) =>
+        appointment.startDate.getFullYear() ===
+          newAppointment.startDate.getFullYear() &&
+        appointment.startDate.getMonth() ===
+          newAppointment.startDate.getMonth() &&
+        appointment.startDate.getDate() ===
+          newAppointment.startDate.getDate() &&
+        appointment.endDate.getDate() === newAppointment.endDate.getDate(),
+    );
+
+    const alertBool = sameDayAppointments.some(
+      (appointment) =>
+        appointment.startDate < newAppointment.endDate &&
+        appointment.endDate > newAppointment.startDate,
+    );
+    setAlert(alertBool);
+    return alertBool;
+  };
+
   const commitChanges = ({ added, changed, deleted }) => {
     setData((prevData) => {
       let data = prevData;
       if (added) {
-        const startingAddedId =
-          data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, ...added }];
+        if (!checkOverlap(added, data)) {
+          const startingAddedId =
+            data.length > 0 ? data[data.length - 1].id + 1 : 0;
+          data = [...data, { id: startingAddedId, ...added }];
+        } else {
+          setAlert(true);
+        }
       }
       if (changed) {
-        data = data.map((appointment) =>
-          changed[appointment.id]
-            ? { ...appointment, ...changed[appointment.id] }
-            : appointment,
-        );
+        if (!checkOverlap(changed, data)) {
+          data = data.map((appointment) =>
+            changed[appointment.id]
+              ? { ...appointment, ...changed[appointment.id] }
+              : appointment,
+          );
+        }
       }
       if (deleted !== undefined) {
         data = data.filter((appointment) => appointment.id !== deleted);
