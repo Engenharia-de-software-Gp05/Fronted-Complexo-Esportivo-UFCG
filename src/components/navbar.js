5a1d631d022c72199223c1ad611176f986000769
Matheus Victor <matheusvictor.dev@gmail.com>
2024-05-02 00:23:23 -0300

    feat: hide the icons

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
@@ -17,12 +17,13 @@ import ListItemText from "@mui/material/ListItemText";
 import CalendarMonth from "@mui/icons-material/CalendarMonth";
 import Event from "@mui/icons-material/Event";
 import Gradient from "@mui/icons-material/Gradient";
-import SportsVolleyballIcon from '@mui/icons-material/SportsVolleyball';
+import SportsVolleyballIcon from "@mui/icons-material/SportsVolleyball";
 import School from "@mui/icons-material/School";
 import Groups from "@mui/icons-material/Groups";
 import Logout from "@mui/icons-material/Logout";
-import HowToRegIcon from '@mui/icons-material/HowToReg';
+import HowToRegIcon from "@mui/icons-material/HowToReg";
 import { Link } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const drawerWidth = 240;
 
@@ -30,21 +31,21 @@ const openedMixin = (theme) => ({
   width: drawerWidth,
   transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
+    duration: theme.transitions.duration.enteringScreen
   }),
-  overflowX: "hidden",
+  overflowX: "hidden"
 });
 
 const closedMixin = (theme) => ({
   transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
+    duration: theme.transitions.duration.leavingScreen
   }),
   overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
   [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
+    width: `calc(${theme.spacing(8)} + 1px)`
+  }
 });
 
 const DrawerHeader = styled("div")(({ theme }) => ({
@@ -53,29 +54,29 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
+  ...theme.mixins.toolbar
 }));
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== "open"
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
+    duration: theme.transitions.duration.leavingScreen
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
+      duration: theme.transitions.duration.enteringScreen
+    })
+  })
 }));
 
 const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== "open"
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
@@ -83,17 +84,30 @@ const Drawer = styled(MuiDrawer, {
   boxSizing: "border-box",
   ...(open && {
     ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme)
   }),
   ...(!open && {
     ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
+    "& .MuiDrawer-paper": closedMixin(theme)
+  })
 }));
 
 export default function MiniDrawer() {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [userType, setUserType] = useState();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const roles = decodedToken.roles
+        .slice(1, -1)
+        .split(",")
+        .map((role) => role.trim());
+      setUserType(roles.includes("ROLE_ADMIN") ? "ROLE_ADMIN" : "ROLE_USER");
+    }
+  }, []);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -101,6 +115,30 @@ export default function MiniDrawer() {
 
   const handleDrawerClose = () => {
     setOpen(false);
+  };
+
+  const renderButtons = () => {
+    switch (userType) {
+      case "ROLE_USER":
+      case "ROLE_PENDING":
+        return [
+          { text: "Agendar", link: "/scheduler" },
+          { text: "Meus agendamentos", link: "/scheduler" },
+          { text: "Quadras", link: "/list-courts" }
+        ];
+      case "ROLE_ADMIN":
+      default:
+        return [
+          { text: "Agendar", link: "/scheduler" },
+          { text: "Meus agendamentos", link: "/scheduler" },
+          { text: "Quadras", link: "/list-courts" },
+          { text: "Registrar Quadras", link: "/register-court" },
+          { text: "Usuários", link: "/list-users" },
+          { text: "Funcionários", link: "/list-employees" },
+          { text: "Registrar Funcionários", link: "/register-employee" },
+          { text: "Sair", link: "/" }
+        ];
+    }
   };
 
   return (
@@ -150,16 +188,7 @@ export default function MiniDrawer() {
             backgroundColor: "#FFFAFA",
           }}
         >
-          {[
-            { text: "Agendar", link: "/scheduler" },
-            { text: "Meus agendamentos", link: "/scheduler" },
-            { text: "Quadras", link: "/list-courts" },
-            { text: "Registrar Quadras", link: "/register-court"},
-            { text: "Usuários", link: "/list-users" },
-            { text: "Funcionários", link: "/list-employees" },
-            { text: "Registrar Funcionários", link: "/register-employee"},
-            { text: "Sair", link: "/" },
-          ].map(({ text, link }, index) => (
+          {renderButtons().map(({ text, link }, index) => (
             <ListItem key={text} disablePadding sx={{ display: "block" }}>
               <ListItemButton
                 component={Link}
@@ -205,3 +234,4 @@ export default function MiniDrawer() {
     </Box>
   );
 }
+
